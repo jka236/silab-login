3538e19d63598275319f872b2ff8a0532165c966
Jonghyeok K <jka236@sfu.ca>
2022-09-06 19:51:45 -0700

    init command delay

--- project/init-shell.sh
+++ project/init-shell.sh
@@ -5,8 +5,9 @@ export FLASK_DEBUG=1
 
 echo "DB init"
 sleep 10
-flask db init || echo "skip db init"
+flask db init && sleep 10 || echo "skip db init"
 echo "Flask DB init"
-sleep 10
+
+flask db migrate && sleep 5
 flask db upgrade
 flask run --host=0.0.0.0 --port=4000
