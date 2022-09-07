00bfc9a7582f739987b53b7c9d5ae58f8fee1f92
Jonghyeok K <jka236@sfu.ca>
2022-09-06 19:34:15 -0700

    postgres git ignore

--- project/init-shell.sh
+++ project/init-shell.sh
@@ -3,9 +3,8 @@
 export FLASK_APP=project
 export FLASK_DEBUG=1
 
-flask db init || echo "DB init skip"
 echo "DB init"
+sleep 10
 
-flask db migrate
 flask db upgrade
 flask run --host=0.0.0.0 --port=4000
